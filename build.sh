ff687e5a67bd06e6c29f95b79b06639d6d4a3fe7
DavidBugger <bwehfukdavid@gmail.com>
2026-05-18 15:52:18 +0100

    added creation of admin at build

--- build.sh
+++ build.sh
@@ -8,5 +8,19 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]]; then
-  python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='$SUPERUSER_USERNAME').exists() or User.objects.create_superuser('$SUPERUSER_USERNAME', '$SUPERUSER_EMAIL', '$SUPERUSER_PASSWORD')"
+  python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+email = '$SUPERUSER_EMAIL'
+username = '$SUPERUSER_USERNAME'
+name = '${SUPERUSER_NAME:-Admin}'
+password = '$SUPERUSER_PASSWORD'
+if not User.objects.filter(email=email).exists():
+    u = User(email=email, username=username, name=name, role='system_admin', is_staff=True, is_superuser=True)
+    u.set_password(password)
+    u.save()
+    print('Superuser created successfully.')
+else:
+    print('Superuser already exists.')
+"
 fi
